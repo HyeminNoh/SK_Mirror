e21b178c42986c40a6f4372ee4bc923ea5df2587
HyeminNoh <shrmffkfk@naver.com>
2019-05-24 03:12:20 +0900

    헤어스타일링 지원 스마트미러 웹앱

--- GC_Server/main.py
+++ GC_Server/main.py
@@ -1,18 +1,5 @@
-# Copyright 2015 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#헤어스타일링 지원 스마트미러 flask app
 
-# [START gae_flex_quickstart]
 import logging
 import requests
 import ImageMatching as ImageMatching
